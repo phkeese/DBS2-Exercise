d2474e1766db8ac0d6065586bc9bd81df97cb6f1
Philipp Keese <philipp.keese@student.hpi.de>
2022-11-18 11:31:49 +0100

    sort blocks on disk

--- src/main/java/exercise1/TPMMSJava.java
+++ src/main/java/exercise1/TPMMSJava.java
@@ -2,16 +2,14 @@ package exercise1;
 
 import com.google.common.collect.Lists;
 import de.hpi.dbs2.ChosenImplementation;
-import de.hpi.dbs2.dbms.Block;
-import de.hpi.dbs2.dbms.BlockManager;
-import de.hpi.dbs2.dbms.BlockOutput;
-import de.hpi.dbs2.dbms.Relation;
+import de.hpi.dbs2.dbms.*;
 import de.hpi.dbs2.dbms.utils.BlockSorter;
 import de.hpi.dbs2.exercise1.SortOperation;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 @ChosenImplementation(true)
 public class TPMMSJava extends SortOperation {
@@ -45,6 +43,31 @@ public class TPMMSJava extends SortOperation {
         int listCount = totalRelationSize / partitionSize + (totalRelationSize % partitionSize > 0 ? 1 : 0);
         List<Block> blockList = Lists.newArrayList(relation.iterator());
         List<List<Block>> lists = Lists.partition(blockList, partitionSize);
+
+        // Sort each list and store back on disk
+        lists.forEach(new Consumer<List<Block>>() {
+            @Override
+            public void accept(List<Block> blocks) {
+                // Load list into memory
+                for (int i = 0; i < blocks.size(); i++) {
+                    blocks.set(i, bm.load(blocks.get(i)));
+                }
+
+                // Sort
+                BlockSorter.INSTANCE.sort(
+                        relation,
+                        blocks,
+                        relation.getColumns().getColumnComparator(getSortColumnIndex())
+                );
+
+                // Write back to disk
+                for (int i = 0; i < blocks.size(); i++) {
+                    Block ramBlock = blocks.get(i);
+                    Block diskBlock = bm.release(ramBlock, true);
+                    blocks.set(i, diskBlock);
+                }
+            }
+        });
 
 
         // Phase 2: Merge!
