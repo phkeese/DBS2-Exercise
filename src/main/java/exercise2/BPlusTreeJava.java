04e331ca78c8bf6c2185b2957a925dbf25e4c2b3
Shieka <57802017+Shieka@users.noreply.github.com>
2022-12-02 10:54:52 +0100

    hehe
    Yeah Stuff!!!!

--- src/main/java/exercise2/BPlusTreeJava.java
+++ src/main/java/exercise2/BPlusTreeJava.java
@@ -5,6 +5,8 @@ import de.hpi.dbs2.exercise2.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Stack;
+
 /**
  * This is the B+-Tree implementation you will work on.
  * Your task is to implement the insert-operation.
@@ -23,17 +25,44 @@ public class BPlusTreeJava extends AbstractBPlusTree {
     @Nullable
     @Override
     public ValueReference insert(@NotNull Integer key, @NotNull ValueReference value) {
-        throw new UnsupportedOperationException("~~~ your implementation here ~~~");
+        //   It is a good idea to track the "path" to the LeafNode in a Stack or something alike.
+        Stack<InnerNode> path = new Stack<>();
 
         // Find LeafNode in which the key has to be inserted.
-        //   It is a good idea to track the "path" to the LeafNode in a Stack or something alike.
+        BPlusTreeNode<?> current = rootNode;
+
+        while(current.references.length == current.order){
+            InnerNode innerNode  = (InnerNode)current;
+            path.push(innerNode);
+            current = innerNode.selectChild(key);
+        }
+        LeafNode leafNode  = (LeafNode) current;
+
         // Does the key already exist? Overwrite!
         //   leafNode.references[pos] = value;
         //   But remember return the old value!
+
+        for(int i = 0; i < leafNode.keys.length; i++){
+            if(leafNode.keys[i] == key){
+                ValueReference oldValue = leafNode.references[i];
+                leafNode.references[i] = value;
+                return oldValue;
+            }
+        }
+
         // New key - Is there still space?
         //   leafNode.keys[pos] = key;
         //   leafNode.references[pos] = value;
         //   Don't forget to update the parent keys and so on...
+
+        for(int i = 0; i < leafNode.keys.length; i++){
+            if(leafNode.keys[i] == null){
+                leafNode.keys[i] = key;
+                leafNode.references[i] = value;
+                return value;
+            }
+        }
+
         // Otherwise
         //   Split the LeafNode in two!
         //   Is parent node root?
@@ -41,6 +70,9 @@ public class BPlusTreeJava extends AbstractBPlusTree {
         //   Was node instanceof LeafNode?
         //     update parentNode.keys[?] = ...
         //   Don't forget to update the parent keys and so on...
+
+        int rightHalfCount = leafNode.n/2;
+        LeafNode rightLeafNode = new LeafNode(leafNode.order);
 
         // Check out the exercise slides for a flow chart of this logic.
         // If you feel stuck, try to draw what you want to do and
