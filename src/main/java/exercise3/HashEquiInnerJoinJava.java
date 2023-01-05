1ee12b9ee9a7a2dc22d730d930fbc1a11027eebf
Philipp Keese <philipp.keese@student.hpi.de>
2023-01-05 15:01:44 +0100

    blubs

--- src/main/java/exercise3/HashEquiInnerJoinJava.java
+++ src/main/java/exercise3/HashEquiInnerJoinJava.java
@@ -6,6 +6,9 @@ import de.hpi.dbs2.exercise3.InnerJoinOperation;
 import de.hpi.dbs2.exercise3.JoinAttributePair;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 @ChosenImplementation(true)
 public class HashEquiInnerJoinJava extends InnerJoinOperation {
 
@@ -27,6 +30,8 @@ public class HashEquiInnerJoinJava extends InnerJoinOperation {
 		@NotNull Relation leftInputRelation, @NotNull Relation rightInputRelation,
 		@NotNull Relation outputRelation
 	) {
+		// TODO:
+		//  - calculate a sensible bucket count
 		int bucketCount = getBlockManager().getFreeBlocks() - 1;
 
 		// Test wether this join can be executed min(B(S),B(R)) < (M-1)^2
@@ -36,10 +41,125 @@ public class HashEquiInnerJoinJava extends InnerJoinOperation {
 		}
 
 		// TODO:
-		//  - calculate a sensible bucket count
 		//  - hash relation
-		//  - join hashed blocks
+		Bucket[] leftBuckets = hashRelation(leftInputRelation, bucketCount);
+		Bucket[] rightBuckets = hashRelation(rightInputRelation, bucketCount);
 
-		throw new UnsupportedOperationException();
+		// TODO:
+		//  - join hashed blocks
+		TupleAppender tupleAppender = new TupleAppender(outputRelation.getBlockOutput());
+		for (int bucketIndex = 0; bucketIndex < bucketCount; bucketIndex++) {
+			joinBuckets(leftBuckets[bucketIndex], rightBuckets[bucketIndex],outputRelation, tupleAppender);
+		}
 	}
+
+	private void joinBuckets(Bucket leftBucket, Bucket rightBucket,  Relation outputRelation, TupleAppender tupleAppender) {
+		Block[] leftBlocks = leftBucket.load();
+		for (int i = 0; i < rightBucket.size(); i++) {
+			Block rightBlock = rightBucket.load(i);
+			for (Block leftBlock : leftBlocks) {
+					joinBlocks(
+							leftBlock,
+							rightBlock,
+							outputRelation.getColumns(),
+							tupleAppender
+					);
+			}
+			getBlockManager().release(rightBlock, false);
+		}
+		for (int i =0; i < leftBucket.size(); i++) {
+			getBlockManager().release(leftBlocks[i], false);
+		}
+	}
+
+	private Bucket[] hashRelation(@NotNull Relation relation, int bucketCount) {
+		Bucket buckets[] = new Bucket[bucketCount];
+		for (int i = 0; i < bucketCount; i++) {
+			buckets[i] = new Bucket();
+		}
+		for (Block blockRef : relation) {
+			Block block = getBlockManager().load(blockRef);
+			for (Tuple tuple : block) {
+				int hash = tuple.get(getJoinAttributePair().getLeftColumnIndex()).hashCode();
+				int bucketIndex = hash % bucketCount;
+				// Insert into bucket
+				buckets[bucketIndex].add(tuple);
+			}
+			getBlockManager().release(block, false);
+		}
+		// Release all buckets to disk
+		for (Bucket bucket : buckets) {
+			bucket.release();
+		}
+		return buckets;
+	}
+
+
+	class TupleAppender implements AutoCloseable, Consumer<Tuple> {
+
+		BlockOutput blockOutput;
+
+		TupleAppender(BlockOutput blockOutput) {
+			this.blockOutput = blockOutput;
+		}
+
+		Block outputBlock = getBlockManager().allocate(true);
+
+		@Override
+		public void accept(Tuple tuple) {
+			if(outputBlock.isFull()) {
+				blockOutput.move(outputBlock);
+				outputBlock = getBlockManager().allocate(true);
+			}
+			outputBlock.append(tuple);
+		}
+
+		@Override
+		public void close() {
+			if(!outputBlock.isEmpty()) {
+				blockOutput.move(outputBlock);
+			} else {
+				getBlockManager().release(outputBlock, false);
+			}
+		}
+	}
+
+	class Bucket {
+
+		Block currentBlock = getBlockManager().allocate(true);
+		ArrayList<Block> blocks = new ArrayList<>();
+
+		public void add(Tuple tuple) {
+			if(currentBlock.isFull()) {
+				// Save to disk and remember block
+				blocks.add(getBlockManager().release(currentBlock, true));
+				currentBlock = getBlockManager().allocate(true);
+			}
+			currentBlock.append(tuple);
+		}
+
+		public void release() {
+			if(!currentBlock.isEmpty()) {
+				blocks.add(getBlockManager().release(currentBlock, true));
+			} else {
+				getBlockManager().release(currentBlock, false);
+			}
+		}
+
+		public Block[] load()  {
+			Block[] loaded = new Block[blocks.size()];
+			for (int i = 0; i < blocks.size(); i++) {
+				loaded[i] = getBlockManager().load(blocks.get(i));
+			}
+			return loaded;
+		}
+
+		public Block load(int index) {
+			return getBlockManager().load(blocks.get(index));
+		}
+
+		public int size() {
+			return blocks.size();
+		}
+	};
 }
